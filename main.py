9e53a713c261c4ad4b9d0bcfe66888150a4cca4e
Andrew-Chek <andrii.chekyrda@gmail.com>
2021-02-28 16:40:03 +0200

    lab finished

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from math import *
+from try_parse.utils import ParseUtils
+
 x = input('Enter num x: ')
-x = int(x)
 y = input('Enter num y: ')
-y = int(y)
-z = pow(cos(x), 2) + pow(sin(y), 2)
-print("z is: ", z)
+try:
+    x = int(x)
+    y = int(y)
+except:
+    print("Error: number x or y isn't correct")
+else:
+    z = pow(cos(x), 2) + pow(sin(y), 2)
+    print("z is: ", z)
 
 N = input('Enter num N: ')
-N = int(N)
-s = 0
-if N > 0:
-    for i in range(N+1):
-        s = s + pow(i, 2)
-    print("sum of elements in square is: ", s)
+try:
+    N = int(N)
+except:
+    print("Error: number N isn't correct")
 else:
-    print("Error: number N can't be negative")
+    s = 0
+    if N > 0:
+        for i in range(N + 1):
+            s = s + pow(i, 2)
+        print("sum of elements in square is: ", s)
+    else:
+        print("Error: number N can't be negative")
 
 a = [-17, -1, 12, 4, 3, -12, 10, 6]
 print(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7])
